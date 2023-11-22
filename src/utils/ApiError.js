6f054259ec4d68294df8ea14a3916ae5a894ff2f
Jignesh Kiri <jignesh.kiri@astralltd.com>
2023-11-22 14:55:19 +0530

    router and conterller configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class ApiHandler extends Error {
+class ApiError extends Error {
   constructor(
     statusCode,
     message = "Something went wrong.",
@@ -20,4 +20,4 @@ class ApiHandler extends Error {
   }
 }
 
-export { ApiHandler };
+export { ApiError };
